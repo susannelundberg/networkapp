2456a9491d5bdbbc60addcef7bdbf7d29152b0d5
susannelundberg <lundbergsusanne@outlook.com>
2026-08-31 12:56:54 +0200

    Seeding of roles and admin-user at startup

--- src/NetworkApp.Api/Program.cs
+++ src/NetworkApp.Api/Program.cs
@@ -1,5 +1,9 @@
 using NetworkApp.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using NetworkApp.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using NetworkApp.Application;
+using NetworkApp.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +20,56 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+builder.Services.AddScoped<ITokenService, TokenService>();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+
+    if (!await roleManager.RoleExistsAsync("Admin"))
+    {
+        await roleManager.CreateAsync(new IdentityRole("Admin"));
+    }
+
+    if (!await roleManager.RoleExistsAsync("User"))
+    {
+        await roleManager.CreateAsync(new IdentityRole("User"));
+    }
+
+    if (!await roleManager.RoleExistsAsync("Manager"))
+    {
+        await roleManager.CreateAsync(new IdentityRole("Manager"));
+    }
+
+    var result = await userManager.FindByEmailAsync("admin@admin123.se");
+
+    if (result is null)
+    {
+       var user = new User()
+       {
+           FirstName = "Förnamn",
+           LastName = "Efternamn",
+           Email = "admin@admin123.se",
+           UserName = "admin@admin123.se"
+       };
+
+       var admin = await userManager.CreateAsync(user, "Admin123!");
+
+       if (!admin.Succeeded)
+        {
+            Console.WriteLine("Lösenordet behöver ha stor och liten bokstav, en siffra och ett specialtecken");
+        }
+
+       await userManager.AddToRoleAsync(user, "Admin");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
